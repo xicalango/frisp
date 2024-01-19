9a637f818322c1d33906934f3dea63642ae6b6ec
Alexander Weld <weldale@gmail.com>
2024-01-19 13:11:20 +0100

    tokenization

--- libfrisp/src/lib.rs
+++ libfrisp/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, iter::Peekable};
 
 
 #[derive(Debug)]
@@ -8,6 +8,7 @@ pub enum Token {
     Integer(isize),
     Float(f64),
     Symbol(String),
+    String(String),
 }
 
 #[derive(Debug)]
@@ -16,22 +17,26 @@ enum TokenizerState {
     String,
 }
 
-pub struct TokenStream<I> {
-    iter: I,
+pub struct TokenStream<I> 
+where I: Iterator<Item = char> {
+    iter: Peekable<I>,
+    next_token: Option<Token>,
 }
 
-impl<I> TokenStream<I> {
+impl<I> TokenStream<I>
+where I: Iterator<Item = char> {
 
     pub fn new(iter: I) -> TokenStream<I> {
         TokenStream {
-            iter
+            iter: iter.peekable(),
+            next_token: None,
         }
     }
 
 }
 
 impl<I> Debug for TokenStream<I>
-where I: Debug {
+where I: Debug + Iterator<Item = char> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("TokenStream").field("iter", &self.iter).finish()
     }
@@ -42,12 +47,84 @@ where I: Iterator<Item = char> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.iter.next() {
-            Some('(') => Some(Token::ListStart),
-            Some(')') => Some(Token::ListEnd),
-            None => None,
-            e => panic!("eheh {e:?}"),
+        if let Some(t) = self.next_token.take() {
+            return Some(t);
         }
+
+        while let Some(c) = self.iter.next() {
+            match c {
+                w if w.is_whitespace() => continue,
+                '(' => return Some(Token::ListStart),
+                ')' => return Some(Token::ListEnd),
+                '"' => {
+                    let mut buf = String::new();
+                    while let Some(c) = self.iter.next() {
+                        if c == '"' {
+                            return Some(Token::String(buf));
+                        }
+
+                        buf.push(c);
+                    }
+                    panic!("EOF while reading string");
+                },
+                c if c.is_ascii_digit() => {
+                    let mut is_float = false;
+
+                    let mut buf = String::new();
+                    buf.push(c);
+
+                    while let Some(c) = self.iter.next() {
+                        if c.is_whitespace() {
+                            if is_float {
+                                return Some(Token::Float(buf.parse().unwrap()));
+                            } else {
+                                return Some(Token::Integer(buf.parse().unwrap()));
+                            }
+                        } else if c == ')' {
+                            self.next_token.replace(Token::ListEnd);
+
+                            if is_float {
+                                return Some(Token::Float(buf.parse().unwrap()));
+                            } else {
+                                return Some(Token::Integer(buf.parse().unwrap()));
+                            }
+                         } else if c.is_ascii_digit() {
+                            buf.push(c);
+                        } else if c == '.' {
+                            is_float = true;
+                            buf.push(c);
+                        } else {
+                            panic!("invalid digit: {c}");
+                        }
+                    }
+
+                    panic!("EOF while reading number");
+                }
+                c if c.is_ascii_alphabetic() => {
+                    let mut buf = String::new();
+                    buf.push(c);
+                    while let Some(c) = self.iter.next() {
+                        if c.is_whitespace() {
+                            return Some(Token::Symbol(buf));
+                        } else if c == '(' {
+                            self.next_token.replace(Token::ListStart);
+                            return Some(Token::Symbol(buf));
+                        } else if c == ')' {
+                            self.next_token.replace(Token::ListEnd);
+                            return Some(Token::Symbol(buf));
+                        } else if c.is_ascii_alphanumeric() {
+                            buf.push(c)
+                        } else {
+                            panic!("invalid symbol char: {c}");
+                        }
+                    }
+
+                    panic!("EOF while reading symbol");
+                }
+                e => panic!("eheh {e:?}"),
+            }
+        }
+        return None;
     }
 
 }
@@ -58,7 +135,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let stream = TokenStream::new("(())".chars());
+        let stream = TokenStream::new("(cdr(1)(2)(\"asd\"))".chars());
 
         for t in stream {
             println!("{t:?}");
