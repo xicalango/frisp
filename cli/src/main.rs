3882240f499ac1ff5cda5c30e64ac84f4fb39929
Alexander Weld <weldale@gmail.com>
2024-02-10 11:58:01 +0100

    clap parsing for cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,29 +1,86 @@
-use std::io::stdin;
+use std::{error::Error, fmt::Display, io::stdin, path::PathBuf};
 
-use libfrisp::{env::Environment, value::Value, Error};
+use libfrisp::{env::Environment, value::Value};
 
-fn main() -> Result<(), Error> {
+use clap::Parser;
 
-    let mut env = Environment::with_default_content();
+#[derive(Debug)]
+enum CliError {
+    FrispError(libfrisp::Error),
+    GenericError(Box<dyn Error>),
+    Quit(isize),
+}
 
-    for arg in std::env::args().skip(1) {
-        libfrisp::eval_file_with_env(arg, &mut env)?;
+impl CliError {
+
+    fn generic_error(error: impl Error + 'static) -> CliError {
+        CliError::GenericError(Box::new(error))
     }
 
+}
+
+impl Display for CliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl Error for CliError {
+
+}
+
+impl From<libfrisp::Error> for CliError {
+    fn from(value: libfrisp::Error) -> Self {
+        CliError::FrispError(value)
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+
+    #[arg(short, long)]
+    include: Vec<String>,
+
+    script: Option<PathBuf>,
+
+}
+
+fn run_repl(env: &mut Environment) -> Result<(), CliError> {
     loop {
         let mut input = String::new();
 
-        let count = stdin().read_line(&mut input).map_err(|e| Error::EvalError(e.to_string()))?;
+        let count = stdin().read_line(&mut input).map_err(CliError::generic_error)?;
 
         if count == 0 {
             break;
         }
 
-        match libfrisp::run_with_env(&input, &mut env) {
+        match libfrisp::run_with_env(&input, env) {
             Ok(Value::Unit) => {},
             Ok(v) => println!("{v}"),
             Err(e) => println!("{e}"),
         }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), CliError> {
+
+    let args = Args::parse();
+
+    let mut env = Environment::with_default_content();
+
+    for include_glob in &args.include {
+        for include in glob::glob(include_glob).map_err(CliError::generic_error)? {
+            libfrisp::eval_file_with_env(include.map_err(CliError::generic_error)?, &mut env)?;
+        }
+    }
+
+    if let Some(script_path) = &args.script {
+        libfrisp::eval_file_with_env(script_path, &mut env)?;
+    } else {
+        run_repl(&mut env)?;
     }
 
     Ok(())
